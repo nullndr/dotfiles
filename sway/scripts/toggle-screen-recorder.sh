3b48d974180f649b45e11fb58306de55278f0f09
Andrea <nullndr@proton.me>
2025-03-06 13:26:29 +0100

    chore(sway): update config

--- sway/scripts/toggle-screen-recorder.sh
+++ sway/scripts/toggle-screen-recorder.sh
@@ -1,10 +1,13 @@
 #!/bin/bash 
 
-pid="$(pgrep "wf-recorder" || pgrep "slurp")"
-status=$?
+main() {
+  local pid="$(pgrep "wf-recorder" || pgrep "slurp")"
 
-if [[ $status != 0 ]]; then 
-  GEO="$(slurp)" &&  wf-recorder -g "$GEO" -f "$HOME/Pictures/$(date +'recording_%Y-%m-%d-%H%M%S.mp4')"
-else 
-  pkill --signal SIGINT wf-recorder
-fi
+  if [[ $? != 0 ]]; then 
+    GEO="$(slurp)" &&  wf-recorder -g "$GEO" -f "$HOME/Pictures/$(date +'recording_%Y-%m-%d-%H%M%S.mp4')"
+  else 
+    pkill --signal SIGINT wf-recorder
+  fi
+}
+
+main
