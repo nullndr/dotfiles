a91a9f34bfb38bd85784ad841b159cc46e9771d2
Andrea <nullndr@proton.me>
2025-07-19 11:47:12 +0200

    chore(waybar): improve custom pacman module

--- .config/waybar/scripts/check-updates.sh
+++ .config/waybar/scripts/check-updates.sh
@@ -1,7 +1,14 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 main() {
-  checkupdates | wc -l
+  updates=$(checkupdates --nocolor 2>/dev/null)
+  if [[ $? -ne 0 || -z "$updates" ]]; then
+    echo '{"text": "", "tooltip": ""}' | jq --unbuffered --compact-output
+  else
+    numberOfUpdates=$(echo "$updates" | wc -l)
+    tooltip=$(echo "$updates" | sed 's/\n/\r/g' | jq -sR .)
+    echo "{\"text\": \"$numberOfUpdates\", \"tooltip\": $tooltip}" | jq --unbuffered --compact-output
+  fi
 }
 
-main $@
+main "$@"
