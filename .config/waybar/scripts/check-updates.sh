c3ae3440fd4d94790a2edd51dc4e37a1055bea44
Andrea <nullndr@proton.me>
2026-05-15 17:32:32 +0200

    chore(waybar): do not check for updates if no connection

--- .config/waybar/scripts/check-updates.sh
+++ .config/waybar/scripts/check-updates.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+has_network() {
+  ip link show up | grep "state UP"
+}
+
 main() {
-  data=""
+  data='{"text": "", "tooltip": ""}'
+
+  if ! has_network > /dev/null; then
+    echo $data | jq --unbuffered --compact-output
+    return
+  fi
+
   updates=$(checkupdates --nocolor 2>/dev/null)
 
-  if [[ $? -ne 0 || -z "$updates" ]]; then
-    data='{"text": "", "tooltip": ""}'
-  else
+  if [[ $? -eq 0 && -n "$updates" ]]; then
     numberOfUpdates=$(echo "$updates" | wc -l)
     tooltip=$(echo "$updates" | sed 's/\n/\r/g' | jq -sR .)
-    data="{\"text\": \"$numberOfUpdates\", \"tooltip\": $tooltip}" 
+    data="{\"text\": \"$numberOfUpdates \", \"tooltip\": $tooltip}" 
   fi
 
   echo $data | jq --unbuffered --compact-output
