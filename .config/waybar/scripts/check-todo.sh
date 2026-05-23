acdd2eb1b6c661154f0dc60cccf0e98d261d61a6
Andrea <nullndr@proton.me>
2026-05-23 10:25:48 +0200

    chore(waybar): add custom todo module

---
+++ .config/waybar/scripts/check-todo.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+main() {
+  local text
+  local tooltip
+  local numberOfTodos=`awk 'NF { count++ } END { print count+0 }' "$HOME/.todo/todo.txt"`
+
+  if (( "$numberOfTodos" == 1 )); then
+    local todo=$(todo.sh ls | head -n1 | sed 's/^[0-9]\+[[:space:]]*//' | cut -d' ' -f1-7)
+    [[ $(wc -w <<< "$todo") -ge 7 ]] && todo+="…"
+
+    text="<span><b>$numberOfTodos</b></span> "
+    tooltip="TODO: $todo"
+  elif (( "$numberOfTodos" != 0 )); then
+    text="<span><b>$numberOfTodos</b></span> "
+    tooltip="You have a bunch of TODOs to complete"
+  fi
+
+  jq --unbuffered -cn --arg "text" "$text" --arg "tooltip" "$tooltip" '{text:$text, tooltip:$tooltip}'
+}
+
+main "$@"
+
